65582618cb13e7cb711a75434b468152a08cf3be
Alex Solomon <alexandersolomonas@gmail.com>
2023-02-19 03:44:23 +0000

    Websocket server now uses same AWS server as the site hosted on

---
+++ echoSocket.js
@@ -0,0 +1,15 @@
+import WebSocket, { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  console.log('new connection');
+  ws.on('message', function message(data, isBinary) {
+    console.log(data);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
